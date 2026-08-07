170068311ecd34a1699b187ce9b280db9b4fe876
SandhiyaR1 <sandhiyaraj005@gmail.com>
2026-08-08 03:30:06 +0530

    streamlit deployment

--- models/llm.py
+++ models/llm.py
@@ -1,7 +1,6 @@
 import os
-
+import streamlit as st
 from dotenv import load_dotenv
-
 from langchain_groq import ChatGroq
 
 load_dotenv()
@@ -9,10 +8,20 @@ load_dotenv()
 
 def get_llm():
 
-    llm = ChatGroq(
+    api_key = None
+
+    # Streamlit Cloud
+    try:
+        api_key = st.secrets["GROQ_API_KEY"]
+    except Exception:
+        pass
+
+    # Local .env
+    if not api_key:
+        api_key = os.getenv("GROQ_API_KEY")
+
+    return ChatGroq(
         model="llama-3.3-70b-versatile",
         temperature=0,
-        api_key=os.getenv("GROQ_API_KEY"),
+        api_key=api_key,
     )
-
-    return llm
